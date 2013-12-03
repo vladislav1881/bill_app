95a098d0f4ee53fffc924508e0489ef705fbd696
Vlad <va.kuznetsov1881@gmail.com>
2013-12-03 10:16:45 +0700

    Finish layout and routes again

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ BillApp::Application.routes.draw do
   get "users/new"
   
   root  'static_pages#home'
-  
   match '/signup', to: 'users#new', via: 'get'
   match '/help', to: 'static_pages#help', via: 'get'
   match '/about', to: 'static_pages#about', via: 'get'
