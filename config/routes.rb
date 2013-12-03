97b8eaf0c97ddd2710df1e46366b2cf538fca1cd
Vlad <va.kuznetsov1881@gmail.com>
2013-12-03 07:12:15 +0700

    Version 1

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 BillApp::Application.routes.draw do
+  get "users/new"
+  
   root  'static_pages#home'
+  match '/signup', to: 'static_pages#signup', via: 'get'
   match '/help', to: 'static_pages#help', via: 'get'
   match '/about', to: 'static_pages#about', via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
