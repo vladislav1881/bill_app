60643d5a1a4d396679cec924c5de4508f8d445e8
Vlad <va.kuznetsov1881@gmail.com>
2013-12-03 07:56:31 +0700

    Lost files

---
+++ spec/requests/user_pages_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe "User Pages" do
+  
+  subject { page }
+
+  describe "sing up page" do 
+    before { visit signup_path }
+
+    it { should have_content('Sign up') }
+    it { should have_title(full_title('Sign up')) } 
+   end 
+end
