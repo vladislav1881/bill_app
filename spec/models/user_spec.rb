547198c8e46dd3664c7a76bb527df5ddda598d0d
Vlad <va.kuznetsov1881@gmail.com>
2013-12-06 19:36:24 -0800

    Make a basic user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -76,6 +76,17 @@ describe User do
 
     it { should_not be_valid }
   end
+
+  describe "email address with mixed case" do
+    let(:mixed_case_email) { "Foo@ExAMPle.CoM" }
+
+    it "should be saved as all lower-case" do
+      @user.email = mixed_case_email
+      @user.save
+      expect(@user.reload.email).to eq mixed_case_email.downcase
+    end
+  end
+  
   describe "with a password that's too short" do
     before { @user.password = @user.password_confirmation = "a" * 5 }
     it { should be_invalid }
