6bfe662ccf7cc62efc99dda274f8e807517ed60f
Vlad <vlad@ubuntu.(none)>
2014-01-05 20:31:59 +0700

    Multilingvo reailization

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -47,14 +47,14 @@ class UsersController < ApplicationController
   end
   
   def following
-    @title = "Following"
+    @title = t(:followings)
     @user = User.find(params[:id])
     @users = @user.followed_users.paginate(page: params[:page])
     render 'show_follow'
   end
 
   def followers
-    @title = "Followers"
+    @title = t(:followers)
     @user = User.find(params[:id])
     @users = @user.followers.paginate(page: params[:page])
     render 'show_follow'
