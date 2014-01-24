c8b274d809a7e218e1684bf0f32797612dd66585
Yury Kotlyarov <yura@brainhouse.ru>
2014-01-24 10:58:23 +0100

    new style & qfixes

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,7 @@ class User < ActiveRecord::Base
                              class_name: "Match"
   has_many :matches_by_user_invited, foreign_key: "invited_id",
                              class_name: "Match"
-  has_attached_file :avatar, :styles => { :medium => "130x130>", :thumb => "100x100>", xs: "50x50#", xxs: "30x30#" }, 
+  has_attached_file :avatar, :styles => { :large => '300x>', :medium => "130x130>", :thumb => "100x100>", xs: "50x50#", xxs: "30x30#" }, 
                     :default_url => "/images/missing_:style.png"
 # to implement it with new avatars run in console: User.all.each { |u| u.avatar.reprocess! if u.avatar? }
 
