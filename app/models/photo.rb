1326c03484a64aff98996efda9466b5cb44683c4
Yury Kotlyarov <yura@brainhouse.ru>
2014-01-30 08:22:48 +0100

    last tweaks

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,5 +2,7 @@ class Photo < ActiveRecord::Base
   belongs_to :user
   belongs_to :micropost
 
+  validates :image, attachment_presence: true
+
   has_attached_file :image, styles: { large: '620x400>', medium: '300x200>', medium_square: '200x200#', small: '100x100#' }
 end
